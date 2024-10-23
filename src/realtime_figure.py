41bb6cda4c5b61ccea50b9f8e1716eb6ed8fe179
Karl Velazquez <93351243+karlvelazquez@users.noreply.github.com>
2024-10-22 23:31:35 -0400

    Refactored realtime_figure to use factory pattern and base class + implementation subclasses pattern

--- src/realtime_figure.py
+++ src/realtime_figure.py
@@ -1,51 +1,34 @@
 from typing import List, Optional, Tuple, Dict, Any
 from numbers import Number
-from IPython.display import clear_output
-import matplotlib
-import matplotlib.axes
-import matplotlib.lines
 from matplotlib import pyplot as plt
 import numpy as np
+from IPython.display import clear_output
 
 class RealtimeFigure:
-    def _verify_mode(self, required_mode: str):
-        if self.mode != required_mode:
-            raise ValueError(f'Mode must be {required_mode} to run, not {self.mode}.')
+    def __init__(
+        self,
+        rows: int = 1,
+        columns: int = 1,
+        subplot_options_set: Optional[List[Optional[Dict[str, Any]]]] = None,
+    ):
+        for name, value in [('rows', rows),
+                            ('columns', columns)]:
+            if not isinstance(value, int) or value < 1:
+                raise ValueError(f"Invalid value for '{name}': {value}. Must be a counting number.")
 
-    def __init__(self, rows: int = 1, columns: int = 1, subplot_options_set: Optional[List[Optional[Dict[str, Any]]]] = None, mode: str = 'inline'):
         self.rows = rows
         self.columns = columns
-        self.subplot_options_set = subplot_options_set
-        self.mode = mode
 
-        if self.mode == 'inline':
-            self._init_inline_mode()
-        elif self.mode == 'window':
-            self._init_window_mode()
+        self.subplot_options_set = subplot_options_set if subplot_options_set is not None else []
 
-    def _init_inline_mode(self):
-        self._verify_mode('inline')
+    def subplots_num(self):
+        return self.rows * self.columns
 
-        plt.switch_backend('inline')
-
-    def _init_window_mode(self):
-        self._verify_mode('window')
-
-        plt.switch_backend('TkAgg')
-        plt.ion()
-
-        self._generate_subplots()
-
-        self.fig.canvas.manager.window.wm_title("RealtimeFigure")
-
-    def _generate_subplots(self):
+    def init_subplots(self):
         self.fig, axes = plt.subplots(self.rows, self.columns)
-        self.axes = axes.ravel() if self.rows * self.columns > 1 else np.asarray([axes])
+        self.axes = axes.ravel() if self.subplots_num() > 1 else np.asarray([axes])
 
-    def update(self, data_sets: List[Optional[Tuple[List[Number], List[Number]]]]):
-        if self.mode == 'inline':
-            self._generate_subplots()
-
+    def update_subplots(self, data_sets: List[Optional[Tuple[List[Number], List[Number]]]]):
         for i, (data_set, ax) in enumerate(zip(data_sets, self.axes)):
             if data_set == None:
                 continue
@@ -54,42 +37,87 @@ class RealtimeFigure:
 
             ax.clear()
 
+            if len(x) == 0 or len(y) == 0:
+                continue
+
             lines = ax.plot(x, y, color='black')
 
             ax.grid(True)
 
-            if self.subplot_options_set and self.subplot_options_set[i]:
-                self._apply_subplot_options(ax, lines, self.subplot_options_set[i])
+            if self.subplot_options_set and i < len(self.subplot_options_set) and self.subplot_options_set[i]:
+                subplot_option_methods = {
+                    "title": ax.set_title,
+                    "xlabel": ax.set_xlabel,
+                    "ylabel": ax.set_ylabel,
+                    "xlim": ax.set_xlim,
+                    "ylim": ax.set_ylim,
+                    "colors": lambda colors: [line.set_color(color) for line, color in zip(lines, colors)]
+                }
+
+                for option, value in self.subplot_options_set[i].items():
+                    if option in subplot_option_methods:
+                        subplot_option_methods[option](value)
 
         self.fig.tight_layout()
 
-        if self.mode == 'inline':
-            self._render_inline_mode()
-        elif self.mode == 'window':
-            self._render_window_mode()
+    def render(self):
+        raise NotImplementedError("Subclasses should implement the render method.")
 
-    def _apply_subplot_options(self, ax: matplotlib.axes.Axes, lines: List[matplotlib.lines.Line2D], subplot_options: Dict[str, Any]):
-        subplot_option_methods = {
-            "title": ax.set_title,
-            "xlabel": ax.set_xlabel,
-            "ylabel": ax.set_ylabel,
-            "xlim": ax.set_xlim,
-            "ylim": ax.set_ylim,
-            "colors": lambda colors: [line.set_color(color) for line, color in zip(lines, colors)]
-        }
+    def update(self, data_sets: List[Optional[Tuple[List[Number], List[Number]]]]):
+        self.update_subplots(data_sets)
+        self.render()
 
-        for option, value in subplot_options.items():
-            if option in subplot_option_methods:
-                subplot_option_methods[option](value)
+class RealtimeFigureInline(RealtimeFigure):
+    def __init__(
+        self,
+        rows: int = 1,
+        columns: int = 1,
+        subplot_options_set: Optional[List[Optional[Dict[str, Any]]]] = None,
+    ):
+        super().__init__(rows, columns, subplot_options_set)
+        plt.switch_backend('module://matplotlib_inline.backend_inline')
 
-    def _render_inline_mode(self):
-        self._verify_mode('inline')
-
+    def render(self):
         clear_output(wait=True)
         plt.show(block=False)
-    
-    def _render_window_mode(self):
-        self._verify_mode('window')
 
+    def update(self, data_sets: List[Optional[Tuple[List[Number], List[Number]]]]):
+        self.init_subplots()
+        super().update(data_sets)
+
+class RealtimeFigureWindow(RealtimeFigure):
+    def __init__(
+        self,
+        rows: int = 1,
+        columns: int = 1,
+        subplot_options_set: Optional[List[Optional[Dict[str, Any]]]] = None,
+    ):
+        super().__init__(rows, columns, subplot_options_set)
+        plt.switch_backend('TkAgg')
+        plt.ion()
+        self.init_subplots()
+        self.fig.canvas.manager.window.wm_title("RealtimeFigure")
+
+    def render(self):
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
+
+_mode_to_realtime_figure_subclass_map: Dict[str, RealtimeFigure] = {
+    'inline': RealtimeFigureInline,
+    'window': RealtimeFigureWindow,
+}
+
+_modes = set(_mode_to_realtime_figure_subclass_map.keys())
+
+_modes_string = ', '.join(f"'{mode}'" for mode in _modes)
+
+def create(
+        rows: int = 1,
+        columns: int = 1,
+        subplot_options_set: Optional[List[Optional[Dict[str, Any]]]] = None,
+        mode: str = 'inline'
+    ) -> RealtimeFigure:
+    if mode in _mode_to_realtime_figure_subclass_map:
+        return _mode_to_realtime_figure_subclass_map[mode](rows, columns, subplot_options_set)
+    else:
+        raise ValueError(f"Unsupported mode '{mode}' received. Supported modes are: {_modes_string}.")
