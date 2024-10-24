733aaa3330bece482d0452b721be55aec841247f
Karl Velazquez <93351243+karlvelazquez@users.noreply.github.com>
2024-10-24 16:17:30 -0400

    updated timer; looping_thread to handle stopping threads with loops from other threads;

---
+++ src/looping_thread.py
@@ -0,0 +1,26 @@
+from typing import Any, Callable, Dict, Optional
+import threading
+import traceback
+
+class LoopingThread(threading.Thread):
+    def __init__(
+        self, 
+        loop_method: Callable[[threading.Event, Dict[Any, Any]], None], 
+        loop_method_args: Optional[Dict[Any, Any]] = None, 
+        name: Optional[str] = None
+    ):
+        super().__init__(name=name)
+        self.loop_method = loop_method
+        self.loop_method_args = loop_method_args
+        self.stop_event = threading.Event()
+
+    def run(self):
+        try:
+            self.loop_method(self.stop_event, self.loop_method_args)
+        except Exception as e:
+            print(f'Error occurred in thread {self.name}: {e}')
+            traceback.print_exc()
+            self.stop_event.set()
+
+    def stop(self):
+        self.stop_event.set()
--- src/timer.py
+++ src/timer.py
@@ -3,13 +3,13 @@ import time
 class Timer:
     def __init__(self) -> None:
         self.start_time = time.time()
-        self.t = 0.0
+        self.last_t = 0.0
     
     def dt(self) -> float:
-        new_t = time.time() - self.start_time
-        dt = new_t - self.t
-        self.t = new_t
+        t = self.t()
+        dt = t - self.last_t
+        self.last_t = t
         return dt
     
-    def get_t(self) -> float:
-        return self.t
+    def t(self) -> float:
+        return time.time() - self.start_time
