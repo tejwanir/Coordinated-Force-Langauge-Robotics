6a7cfd2805c9ed00aab434a734535f8de9b09f35
Karl Velazquez <93351243+karlvelazquez@users.noreply.github.com>
2024-10-25 13:30:07 -0400

    refactored stoppable thread class member variables

--- src/stoppable_thread.py
+++ src/stoppable_thread.py
@@ -5,18 +5,18 @@ import traceback
 class StoppableThread(threading.Thread):
     def __init__(
         self, 
-        loop_method: Callable[[threading.Event, Dict[Any, Any]], None], 
-        loop_method_args: Optional[Dict[Any, Any]] = None, 
+        stoppable_method: Callable[[threading.Event, Dict[Any, Any]], None], 
+        stoppable_method_args: Optional[Dict[Any, Any]] = None, 
         name: Optional[str] = None
     ):
         super().__init__(name=name)
-        self.loop_method = loop_method
-        self.loop_method_args = loop_method_args
+        self.stoppable_method = stoppable_method
+        self.stoppable_method_args = stoppable_method_args
         self.stop_event = threading.Event()
 
     def run(self):
         try:
-            self.loop_method(self.stop_event, self.loop_method_args)
+            self.stoppable_method(self.stop_event, self.stoppable_method_args)
         except Exception as e:
             print(f'Error occurred in thread {self.name}: {e}')
             traceback.print_exc()
