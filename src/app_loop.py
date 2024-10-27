1fe005930026499fccf052914f2a4951278fae90
johnpayne-gc <johnpayne@gentlecare.tech>
2024-10-27 17:44:09 -0400

    fix formatting

--- src/app_loop.py
+++ src/app_loop.py
@@ -1,5 +1,6 @@
 from timer import Timer
 
+
 class AppLoop:
     def __init__(self) -> None:
         self.timer = Timer()
@@ -7,16 +8,16 @@ class AppLoop:
 
     def startup(self) -> None:
         pass
-    
+
     def update(self, t: float, dt: float) -> None:
         pass
-    
+
     def shutdown(self) -> None:
         pass
-    
+
     def stop(self) -> None:
         self.running = False
-    
+
     def run(self) -> None:
         self.running = True
         self.startup()
