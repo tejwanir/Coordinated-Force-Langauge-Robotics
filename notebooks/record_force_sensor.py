b598048290bf694121930762277fd478e465031a
johnpayne-dev <email@johnpayne.dev>
2024-12-13 10:20:32 -0500

    add turn recording

--- notebooks/record_force_sensor.py
+++ notebooks/record_force_sensor.py
@@ -4,19 +4,28 @@ sys.path.append('src')  # nopep8
 from force_sensor import OptoForceSensor
 import pandas as pd
 import time
+import keyboard
 
 sensor = OptoForceSensor()
 force_axis = []
 t_axis = []
+last_keypress_time = 0
+turn = 1
+print(f'Turn {turn}')
 
 data = []
 
 try:
     while True:
+        if keyboard.is_pressed('enter') and time.time() - last_keypress_time > 1:
+            turn += 1
+            print(f'Turn {turn}')
+            last_keypress_time = time.time()
+
         force, torque = sensor.read()
-        data.append([time.time(), force[0], force[1], force[2],
+        data.append([time.time(), turn, force[0], force[1], force[2],
                      torque[0], torque[1], torque[2]])
 finally:
     data_frame = pd.DataFrame(
-        columns=["time", "fx", "fy", "fz", "tx", "ty", "tz"], data=data)
+        columns=["time", "turn", "fx", "fy", "fz", "tx", "ty", "tz"], data=data)
     data_frame.to_csv(f'{time.time()}.csv')
