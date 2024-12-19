11f2e6cadcba97e69a3dfa5d9ce7416fe645ad03
Karl Velazquez <93351243+karlvelazquez@users.noreply.github.com>
2024-12-18 21:58:40 -0500

    lang2force_trial_data

--- src/phrase_trial_data.py
+++ src/phrase_trial_data.py
@@ -43,13 +43,13 @@ class PhraseTrialData:
         self.force.append(force)
 
     def save(self, index: int) -> None:
-        os.makedirs('phrase_trial_data', exist_ok=True)
+        os.makedirs('lang2force_trial_data', exist_ok=True)
 
         safe_phrase = self.phrase.replace(" ", "_")
 
         file_name = f"{self.user_id}__{index}__{safe_phrase}.pkl"
 
-        file_path = os.path.join('phrase_trial_data', file_name)
+        file_path = os.path.join('lang2force_trial_data', file_name)
 
         with open(file_path, 'wb') as file:
             pickle.dump(self, file)
